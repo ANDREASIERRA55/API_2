969f7ec08d6e2a2f5a4b7e55c8c51f4e53a32856
Andrea103 <andreasierra103@gmail.com>
2023-06-27 12:56:10 +0200

    comprobacion de resultado

--- main.js
+++ main.js
@@ -1,3 +1,81 @@
+// const APIURL = 'https://api.github.com/users/'
+
+// const form = document.getElementById('form');
+// const searchInput = document.getElementById('search');
+// const main = document.getElementById('main');
+
+// form.addEventListener('sumit', function(event) {
+//     event.preventDefault();
+//     const username = searImput.value;
+//     getUser(username);
+// });
+// function getUser(username) {
+//     axios.get(`${APIURL}${username}`)
+//     .then(response => {
+//         const user = response.data;
+//         displayUser(user);
+//     })
+//     .catch(error => {
+//         displayError('User profile not found');
+//     });
+// };
+// function displayUser(user) {
+//     main.innerHTML = `
+//     <div class="user-card">
+//     <img src="${user.avatar_url}" alt="User Avatar">
+//     <h2>${user.name}</h2>
+//     <p>${user.bio}</p>
+//     <ul>
+//         <li>Seguidores: ${user.followers}</li>
+//         <li>Siguiendo: ${user.following}</li>
+//     </ul>
+//     <a href="${user.html_url}" target="_blank">Ver perfil</a>
+//     </div>
+//     `;   
+// };
+
+// function displayError(message) {
+//     main.innerHTML = `<p class="error">${message}</p>`;  
+// }
 const APIURL = 'https://api.github.com/users/'
 
-FormData.APIURL
+const form = document.getElementById('form');
+const searchInput = document.getElementById('search');
+const main = document.getElementById('main');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const username = searchInput.value;
+  getUser(username);
+});
+
+function getUser(username) {
+  axios.get(`${APIURL}${username}`)
+    .then(response => {
+      const user = response.data;
+      displayUser(user);
+    })
+    .catch(error => {
+      console.error(error);
+      displayError('not found');
+    });
+};
+
+function displayUser(user) {
+    main.innerHTML = `
+      <div class="user-card">
+        <img src="${user.avatar_url}" alt="User Avatar">
+        <h2>${user.name}</h2>
+        <p>${user.bio}</p>
+        <ul>
+          <li>Seguidores: ${user.followers}</li>
+          <li>Siguiendo: ${user.following}</li>
+        </ul>
+        <a href="${user.html_url}" target="_blank">Ver perfil</a>
+      </div>
+    `;
+};  
+
+function displayError(message) {
+  main.innerHTML = `<p class="error">${message}</p>`;
+}
